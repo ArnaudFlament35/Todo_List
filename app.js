f2291169e69df9ee2fdc8706ebf604ac9c58bbf3
aflamentTM <aflament@thalasso-saintmalo.com>
2026-01-28 16:26:28 +0100

    Ajoute la fonctionnalité d'ajout de tâches avec un nouveau style pour les boutons et les éléments de liste

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("App is running");
+// D'abord cibler l'élément bouton
+let addButton = document.querySelector('.add_task');
+let listContainer = document.getElementById('task-list');
+// Ecouter l'événement click
+addButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    const newTaskElement = document.createElement('li');
+    newTaskElement.classList.add('task');
+    newTaskElement.innerHTML = `<input type="checkbox" name="task" id="task">
+    <button class="btn-tache">
+      <label for="task">Nouvelle tâche</label>
+    </button>`;
+    listContainer.appendChild(newTaskElement);
+})
+// Prévenir le comportement par défaut du formulaire
